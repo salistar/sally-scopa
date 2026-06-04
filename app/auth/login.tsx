7d043ec6193d92f140598bd16fe4838b67ec2633
idriss <salistarcompany@gmail.com>
2026-06-04 23:36:06 +0100

    feat(phase1-4): rotated keystore untrack + engagement stack

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -29,7 +29,7 @@ const APP_COLOR = APP_CONFIG.primary;
 const APP_NAME = APP_CONFIG.name;
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cardImage = require('../../assets/cards/7O.png');
+const cardImage = require('../../assets/card-hero.png');
 
 export default function LoginScreen() {
   const { t } = useTranslation();
